5c41b45dcc2742fb974aba8fc5f5cc734284d7ad
Vinicius Careli <vcareli@gmail.com>
2026-01-12 10:45:03 -0500

    cap3_2

--- triang_oo/program.java
+++ triang_oo/program.java
@@ -13,19 +13,17 @@ public class program {
 		x.a = scanner.nextDouble();
 		x.b = scanner.nextDouble();
 		x.c = scanner.nextDouble();
-		double p1 = (x.a+x.b+x.c)/2;
-		double area1 = Math.sqrt(p1*(p1-x.a)*(p1-x.b)*(p1-x.c));
 		System.out.println("Digite os comprimentos dos três lados do segundo triângulo:");
 		y.a = scanner.nextDouble();
 		y.b = scanner.nextDouble();
 		y.c = scanner.nextDouble();
-		double p2 = (y.a+y.b+y.c)/2;
-		double area2 = Math.sqrt(p2*(p2-y.a)*(p2-y.b)*(p2-y.c));
+		double areaX = x.area();
+		double areaY = y.area();
 		//IO.println("Triangle Area - Heron Fomule");
-		System.out.printf("Triangle 1 Area: %.2f%n", area1);
-		System.out.printf("Triangle 2 Area: %.2f%n", area2);
+		System.out.printf("Triangle 1 Area: %.2f%n", areaX);
+		System.out.printf("Triangle 2 Area: %.2f%n", areaY);
 		IO.print("Larger Area: ");
-		if (area1 > area2) {
+		if (areaX > areaY) {
 			System.out.println("Triangle 1");
 		} else {
 			System.out.println("Triangle 2");
